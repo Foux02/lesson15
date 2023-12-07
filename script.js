ef5fd743ddc202485dc9ed26a0916f31c8c09335
Jane Lutak <foux02911@gmail.com>
2023-12-07 21:11:11 +0300

    Задание выполнено

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
 'use strict';
 
-const DomElement = function (sel, h, wid, bg, fS) {
-  this.selector = sel;
-  this.height = h;
-  this.width = wid;
-  this.bg = bg;
-  this.fontSize = fS;
+class First {
+  hello() {
+    console.log(`Привет я метод родителя!`);
+  }
+}
 
-  this.newElement = function (text) {
-    this.newText = document.createElement('div');
-    document.body.prepend(this.newText);
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log(`А я наследуемый метод!`);
+  }
+}
 
-    if (this.selector[0] === '.') {
-      this.newText.className = this.selector.substring(1);
-    } else {
-      this.newText.id = this.selector.substring(1);
-    }
+const conclusion = new Second();
 
-    this.newText.append(text);
-
-    this.newText.style.cssText += `
-  height: ${h}px;
-  width: ${wid}px;
-  background: ${bg};
-  font-size: ${fS}px;
-  display: flex;
-  justify-content: center;
-  align-items: center
-`;
-  };
-};
-
-const newBlock = new DomElement('.best', '200', '300', 'green', '55');
-
-newBlock.newElement('I am happy');
+conclusion.hello();
